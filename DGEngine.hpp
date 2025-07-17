b411eb84b0728717c834adc43f6c6ae32d071839
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-07-17 15:34:33 -0400

    Delete DGEngine.hpp

--- DGEngine.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "QuarterLang_Compiler_Pipeline.hpp"
-
-class DGEngine {
-public:
-    std::string toDG(int decimal);
-    int fromDG(const std::string& dg);
-    std::string addDG(const std::string& a, const std::string& b);
-};
